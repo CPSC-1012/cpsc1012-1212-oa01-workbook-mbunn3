1b3624e3d5042555f19921131a9e603652e96b36
Matthew B <mbunn3@nait.ca>
2022-01-06 16:16:06 -0700

    3rd Commit

--- SimpleApp/SimpleApp/Program.cs
+++ SimpleApp/SimpleApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!");
 Console.WriteLine("Goodbye...");
+Console.Write("Write?");
+Console.Write("Wrong");
