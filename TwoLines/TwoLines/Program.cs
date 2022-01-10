6bf098f7c05d219f1f66e5584e84a31d8195f5af
Matthew B <mbunn3@nait.ca>
2022-01-10 15:09:54 -0700

    1st Commit

---
+++ TwoLines/TwoLines/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TwoLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1st line!");
+            Console.WriteLine("WHAT! A 2ND LINE!?!?!?!?");
+        }
+    }
+}
