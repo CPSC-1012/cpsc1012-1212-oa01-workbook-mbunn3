5e77df9785fd9756e1e12bebee2f8339394411c4
Matthew B <mbunn3@nait.ca>
2022-01-10 15:22:06 -0700

    Commit 1

---
+++ GreatFun/GreatFun/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GreatFun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wow all this!");
+            Console.WriteLine(" is on ONE Line!?!?!?>!");
+        }
+    }
+}
