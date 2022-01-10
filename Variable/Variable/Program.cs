d7854088684ef2595bf17c3b61b435c3b10907e2
Matthew B <mbunn3@nait.ca>
2022-01-10 15:46:56 -0700

    Commmit 1

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 5;
+            Console.WriteLine("THE NUMBER IS EQUAL TO "+temp+"!?!?!?!?!?");
+        }
+    }
+}
