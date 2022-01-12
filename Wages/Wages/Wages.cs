d523ce478e511dfd8368579ddeaac9757a4fb7ef
Matthew B <mbunn3@nait.ca>
2022-01-12 16:29:22 -0700

    Commit 1

---
+++ Wages/Wages/Wages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Wages
+{
+    internal class Wages
+    {
+        static void Main(string[] args)
+        {
+            double normalWages, overtimeWages, totalWages;
+            double normalPay, overtimePay;
+            double normalHours, overtimeHours;
+
+            Console.WriteLine("What's your normal pay?");
+            normalPay = double.Parse(Console.ReadLine());
+            Console.WriteLine("What were you non-overtime hours?");
+            normalHours = double.Parse(Console.ReadLine());
+
+            normalWages = normalPay * normalHours;
+
+
+            Console.WriteLine("What's your Overtime pay?");
+            overtimePay = double.Parse(Console.ReadLine());
+            Console.WriteLine("What were you Overtime hours?");
+            overtimeHours = double.Parse(Console.ReadLine());
+
+            overtimeWages = overtimePay * overtimeHours;
+
+            totalWages = overtimeWages + normalWages;
+
+            Console.WriteLine($"{"Normal Pay:",-15} {"$"+normalWages,5}");
+            Console.WriteLine($"{"Overtime Pay:",-15} {"$" + overtimeWages,5}");
+            Console.WriteLine($"{"Total Pay:",-15} {"$" + totalWages,5}");
+
+        }
+    }
+}
